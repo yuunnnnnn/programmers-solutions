11863f27a24395ffbdc6a5eadc1531eed98d1544
yuunnnnnn <saltycreamlatte@gmail.com>
2025-01-04 23:47:49 +0900

    250103

---
+++ algorithm-challenges/lv1/짝수와 홀수.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
